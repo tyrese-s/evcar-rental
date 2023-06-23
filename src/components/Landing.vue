eca7e9dab2143d184b9e3d940b4fb311b3ee68bb
tyrese-s <tyrese.sylvester@yahoo.com>
2023-06-23 12:25:19 +0100

    added landing page elements, with minimal styling

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,5 +1,12 @@
-<template>
-    <div>
-        <h1>landing content</h1>
+<template class="container">
+    <div class="row align-items-center">
+        <div class="col-sm d-flex flex-column mt-5 ml-3">
+            <h1 class="font-weight-bold px-5">Save <span class="text-primary">big</span> with our electric vehicle car rental with EV-car rental today</h1>
+            <p class="">Rent the car you want. When you want. Unbeatable prices</p>
+            <p class="">Pick up a fully charged car today.</p>
+        </div>
+        <div class="col-sm">
+            <img src="../assets/tesla-model-3.png" class="img-fluid">
+        </div>
     </div>
 </template>
